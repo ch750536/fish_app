f8277552a8c905dc43602ab398b1e138f752b316
Jason Cooke <jason.cooke01@gmail.com>
2011-07-15 10:57:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FishApp::Application.config.secret_token = '710b087abd1880b7be8e801a3dce192d938db9713705d649fee2040cc5036a86911b6bd47b1a6be962b79bec19df4f6073095c55ff2e3de08a2bc09a3ab7429f'
